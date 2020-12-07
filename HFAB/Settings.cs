2776a8a671e09e77ad8eaa049e9ebd6f3b7b8d1c
00joaand <00jooand@gafe.molndal.se>
2020-12-07 15:30:59 +0100

    Add project files.

---
+++ HFAB/Settings.cs
@@ -0,0 +1,7 @@
+﻿namespace HFAB
+{
+    internal static class Settings
+    {
+        internal static string Database { get; set; } = @"..\..\..\..\..\Net20\2020v47\DB.Browser.for.SQLite-3.12.1-win32\DB Browser for SQLite\HFAB.db";
+    }
+}
